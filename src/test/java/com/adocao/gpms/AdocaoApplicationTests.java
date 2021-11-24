27bdd481213cf65ad0982aa9b64a920caeb18a2a
Nicolas Atmatzides <nicolas_atmatzides@id.uff.br>
2021-11-23 21:33:01 -0300

    second commit

---
+++ src/test/java/com/adocao/gpms/AdocaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adocao.gpms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdocaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
