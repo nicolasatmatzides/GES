328a9b5c1291f94599c6b1ca1d8a33adb5db906d
Nicolas Atmatzides <nicolas_atmatzides@id.uff.br>
2021-12-05 17:56:18 -0300

    inserções sql

--- src/main/resources/db/migration/h2/V1.0.1__insertData.sql
+++ src/main/resources/db/migration/h2/V1.0.1__insertData.sql
@@ -2,3 +2,16 @@ insert into usuario (id, address, age, civil_id, gender, name, active, cpf, emai
 values(1,'rua exemplo', 23, '25696969', 'male', 'usuario Normal', 1, '0000000000', 'exemplo@gmail.com', '$2a$10$86ORR1UDP9neidEPmsHqUOxP9jaiL5CLjVZedkRgxto6tNfnBK0EO', 'ROLE_USER');
 insert into usuario (id, address, age, civil_id, gender, name, active, cpf, email, password, role)
 values(2,'rua exemplo', 23, '25696970', 'male', 'usuario admin  ', 1, '0000000001', 'admim@gmail.com', '$2a$10$86ORR1UDP9neidEPmsHqUOxP9jaiL5CLjVZedkRgxto6tNfnBK0EO', 'ROLE_ADMIN');
+
+insert into crianca  (id, address, age, civil_id, gender, name, adocao_status)
+values(1, 'rua da adoção', 11,'2020202020','MASCULINO','Claudio', 'COMPLETED');
+insert into crianca  (id, address, age, civil_id, gender, name, adocao_status)
+values(2, 'rua da adoção', 10,'20202021','FEMININO','Joana', 'COMPLETED');
+insert into crianca  (id, address, age, civil_id, gender, name, adocao_status)
+values(3, 'rua da adoção', 13,'80808416515','FEMININO','Raquel', 'COMPLETED');
+insert into crianca  (id, address, age, civil_id, gender, name, adocao_status)
+values(5, 'rua Z', 14,'15132150','Maculino','Rodrigo', 'EMPTY');
+insert into crianca  (id, address, age, civil_id, gender, name, adocao_status)
+values(6, 'rua Y', 9,'218044984','FEMININO','Juliana', 'EMPTY');
+insert into crianca  (id, address, age, civil_id, gender, name, adocao_status)
+values(7, 'rua X', 8,'808984','FEMININO','Leilane', 'EMPTY');
