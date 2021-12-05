66033dd0a34d7c8aee975072bcd6f91420b070d5
Nicolas Atmatzides <nicolas_atmatzides@id.uff.br>
2021-12-05 18:16:18 -0300

    finished changes

--- src/main/resources/db/migration/h2/V1.0.1__insertData.sql
+++ src/main/resources/db/migration/h2/V1.0.1__insertData.sql
@@ -1,7 +1,7 @@
 insert into usuario (id, address, age, civil_id, gender, name, active, cpf, email, password, role)
-values(1,'rua exemplo', 23, '25696969', 'male', 'usuario Normal', 1, '0000000000', 'exemplo@gmail.com', '$2a$10$86ORR1UDP9neidEPmsHqUOxP9jaiL5CLjVZedkRgxto6tNfnBK0EO', 'ROLE_USER');
+values(1,'rua exemplo', 23, '25696969', 'male', 'usuario Normal', '1', '0000000000', 'exemplo@gmail.com', '$2a$10$86ORR1UDP9neidEPmsHqUOxP9jaiL5CLjVZedkRgxto6tNfnBK0EO', 'ROLE_USER');
 insert into usuario (id, address, age, civil_id, gender, name, active, cpf, email, password, role)
-values(2,'rua exemplo', 23, '25696970', 'male', 'usuario admin  ', 1, '0000000001', 'admim@gmail.com', '$2a$10$86ORR1UDP9neidEPmsHqUOxP9jaiL5CLjVZedkRgxto6tNfnBK0EO', 'ROLE_ADMIN');
+values(2,'rua exemplo', 23, '25696970', 'male', 'usuario admin  ', '1', '0000000001', 'admim@gmail.com', '$2a$10$86ORR1UDP9neidEPmsHqUOxP9jaiL5CLjVZedkRgxto6tNfnBK0EO', 'ROLE_ADMIN');
 
 insert into crianca  (id, address, age, civil_id, gender, name, adocao_status)
 values(1, 'rua da adoção', 11,'2020202020','MASCULINO','Claudio', 'COMPLETED');
