e9122488d0950e335578acf5b5c757a9e36e3f4f
MateusRomao <mateusromao.ds@gmail.com>
2021-12-06 12:48:24 -0300

    "Correção do arquivo V1.0.1__insertData.sql"

--- src/main/resources/db/migration/h2/V1.0.1__insertData.sql
+++ src/main/resources/db/migration/h2/V1.0.1__insertData.sql
@@ -1,9 +1,9 @@
 insert into usuario (id, address, age, civil_id, gender, name, active, cpf, email, password, role)
-<<<<<<< HEAD
+
 values(1,'rua exemplo', 23, '25696969', 'male', 'usuario Normal', TRUE, '0000000000', 'exemplo@gmail.com', '$2a$10$86ORR1UDP9neidEPmsHqUOxP9jaiL5CLjVZedkRgxto6tNfnBK0EO', 'ROLE_USER');
 insert into usuario (id, address, age, civil_id, gender, name, active, cpf, email, password, role)
 values(2,'rua exemplo', 23, '25696970', 'male', 'usuario admin  ', TRUE, '0000000001', 'admim@gmail.com', '$2a$10$86ORR1UDP9neidEPmsHqUOxP9jaiL5CLjVZedkRgxto6tNfnBK0EO', 'ROLE_ADMIN');
-=======
+
 values(1,'rua exemplo', 23, '25696969', 'male', 'usuario Normal', '1', '0000000000', 'exemplo@gmail.com', '$2a$10$86ORR1UDP9neidEPmsHqUOxP9jaiL5CLjVZedkRgxto6tNfnBK0EO', 'ROLE_USER');
 insert into usuario (id, address, age, civil_id, gender, name, active, cpf, email, password, role)
 values(2,'rua exemplo', 23, '25696970', 'male', 'usuario admin  ', '1', '0000000001', 'admim@gmail.com', '$2a$10$86ORR1UDP9neidEPmsHqUOxP9jaiL5CLjVZedkRgxto6tNfnBK0EO', 'ROLE_ADMIN');
@@ -20,4 +20,4 @@ insert into crianca  (id, address, age, civil_id, gender, name, adocao_status)
 values(6, 'rua Y', 9,'218044984','FEMININO','Juliana', 'EMPTY');
 insert into crianca  (id, address, age, civil_id, gender, name, adocao_status)
 values(7, 'rua X', 8,'808984','FEMININO','Leilane', 'EMPTY');
->>>>>>> 610f37e3967c0cc6399c54f8970083679c655419
+
