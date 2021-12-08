8064d98ee08e000f39db6cfc1cb7108c92432aee
Nicolas Atmatzides <nicolas_atmatzides@id.uff.br>
2021-12-08 17:23:41 -0300

    tirando codigo morto

--- src/main/java/com/adocao/gpms/controller/CriancaController.java
+++ src/main/java/com/adocao/gpms/controller/CriancaController.java
@@ -4,13 +4,13 @@ import com.adocao.gpms.entity.Crianca;
 import com.adocao.gpms.model.CriancaDTO;
 import com.adocao.gpms.security.UsuarioLogadoSession;
 import com.adocao.gpms.service.CriancaService;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
 import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
@@ -56,25 +56,6 @@ public class CriancaController {
 
         return json;
     }
-/*
-    @PostMapping("crianca/cadastra")
-    public String cadastraCrianca(Model model,
-                                 @RequestParam(name = "nome") Optional<String> nome,
-                                   @RequestParam(name = "idade") Optional<String> idade,
-                                  @RequestParam(name = "genero") Optional<String> genero,
-                                  @RequestParam(name = "endereco") Optional<String> endereco,
-                                  @RequestParam(name = "rg") Optional<String> rg) throws JsonProcessingException{
-        CriancaDTO crianca = new CriancaDTO();
-        nome.ifPresent(crianca::setName);
-        idade.ifPresent(crianca::setAge);
-        genero.ifPresent(crianca::setGender);
-        endereco.ifPresent(crianca::setAddress);
-        rg.ifPresent(crianca::setCivilId);
-
-        return criancaService.cadastraCrianca(crianca,model);
-    }
-*/
-
     @PostMapping("crianca/cadastra")
     public String cadastraCrianca(Model model,
                                   @RequestParam(name = "nome") Optional<String> nome,
