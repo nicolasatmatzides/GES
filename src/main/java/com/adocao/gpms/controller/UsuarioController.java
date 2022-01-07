57a577411665ee8a016c68cd65c24bf95753dc57
EstherMattos <silvaesthermattos@hotmail.com>
2022-01-06 19:33:59 -0800

    Cadastro de usuário quase pronto

--- src/main/java/com/adocao/gpms/controller/UsuarioController.java
+++ src/main/java/com/adocao/gpms/controller/UsuarioController.java
@@ -2,19 +2,18 @@ package com.adocao.gpms.controller;
 
 
 import com.adocao.gpms.entity.Usuario;
+import com.adocao.gpms.model.CriancaDTO;
 import com.adocao.gpms.model.login.UsuarioDTO;
 import com.adocao.gpms.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @Controller
 public class UsuarioController {
@@ -27,19 +26,18 @@ public class UsuarioController {
         return "cadastrar.html";
     }
 
-    @PostMapping("/cadastrar")
-    public String cadastrarUsuario(@RequestBody HashMap<String, String> infoUsuario) {
-        if (!infoUsuario.get("senha").equals(infoUsuario.get("confirmacaoSenha"))) {
-            return "/cadastrar";
-        }
-        Usuario usuario = new Usuario();
-        usuario.setName(infoUsuario.get("name"));
-        usuario.setEmail(infoUsuario.get("email"));
-        usuario.setAddress(infoUsuario.get("address"));
-        usuario.setPassword(infoUsuario.get("senha"));
-
-        UsuarioDTO dto = new UsuarioDTO();
-        usuarioService.cadastrarUsuario(dto);
+    @PostMapping("/salvar")
+    public String cadastraUsuario(Model model,
+                                  @RequestParam(name = "name") Optional<String> name,
+                                  @RequestParam(name = "email") Optional<String> email,
+                                  @RequestParam(name = "address") Optional<String> address,
+                                  @RequestParam(name = "senha") Optional<String> senha){
+       UsuarioDTO usuario = new UsuarioDTO();
+        name.ifPresent(usuario::setName);
+        email.ifPresent(usuario::setEmail);
+        address.ifPresent(usuario::setAddress);
+        senha.ifPresent(usuario::setSenha);
+        usuarioService.cadastrarUsuario(usuario);
         return "redirect:/";
     }
 
