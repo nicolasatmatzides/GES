61b20fc2a2074df5b14b9f6dc87917fd3ba63bf0
Mateus Romão <mateusromao.ds@gmail.com>
2022-01-23 20:49:22 -0300

    retorno a versão do dia 18/01/2022-16:45

--- src/main/java/com/adocao/gpms/security/WebSecurityConfig.java
+++ src/main/java/com/adocao/gpms/security/WebSecurityConfig.java
@@ -25,10 +25,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.requiresChannel()
-                .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
-                .requiresSecure();
-
         http.authorizeRequests()
                 .antMatchers("/loginAdm").hasRole("ADMIN")
                 .antMatchers(HttpMethod.GET,"/admin").hasRole("ADMIN")
