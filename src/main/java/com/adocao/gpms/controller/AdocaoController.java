a6fc28ef5efe6533f0d0428527fb6ac3f9b06c45
EstherMattos <silvaesthermattos@hotmail.com>
2022-02-03 23:09:07 -0800

    em andamento

--- src/main/java/com/adocao/gpms/controller/AdocaoController.java
+++ src/main/java/com/adocao/gpms/controller/AdocaoController.java
@@ -1,13 +1,17 @@
 package com.adocao.gpms.controller;
 
+import com.adocao.gpms.entity.Crianca;
 import com.adocao.gpms.security.UsuarioLogadoSession;
 import com.adocao.gpms.service.AdocaoService;
 import com.adocao.gpms.service.CriancaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -19,6 +23,9 @@ public class AdocaoController {
     @Autowired
     private AdocaoService adocaoService;
 
+    @Autowired
+    private CriancaService criancaService;
+
     @PutMapping("/adocaoEmAndamento")
     public String adoteCriançaEmpty(@RequestParam(name = "id")Optional<String> id) throws Exception {
 
@@ -29,6 +36,13 @@ public class AdocaoController {
     public String adoteCriançaCompleted(@RequestParam(name = "id")Optional<String> id) throws Exception {
 
         return adocaoService.concluiAdocao(Long.parseLong(String.valueOf(id)),usuarioLogadoSession);
+    }
+
+    @GetMapping("/paginaAdocao")
+    public String paginaAdocao(Model model){
+        List<Crianca> criancaList = criancaService.listarCriancaSucesso();
+        model.addAttribute("criancas", criancaList);
+        return "user/minhasAdocoes.html";
     }
 
 }
