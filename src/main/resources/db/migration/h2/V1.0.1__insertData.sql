a8ce80c25bc0f1712294efe92a9a71cabff340e0
EstherMattos <silvaesthermattos@hotmail.com>
2022-02-07 06:37:14 -0800

    esperando nicolas

--- src/main/resources/db/migration/h2/V1.0.1__insertData.sql
+++ src/main/resources/db/migration/h2/V1.0.1__insertData.sql
@@ -3,25 +3,24 @@ values(1,'rua exemplo', 23, '25696969', 'male', 'usuario Normal', '1', '00000000
 insert into usuario (id, address, age, civil_id, gender, name, active, cpf, email, password, role)
 values(2,'rua exemplo', 23, '25696970', 'male', 'usuario admin  ', '1', '0000000001', 'admim@gmail.com', '$2a$10$86ORR1UDP9neidEPmsHqUOxP9jaiL5CLjVZedkRgxto6tNfnBK0EO', 'ROLE_ADMIN');
 
-insert into crianca  (id, address, age, civil_id, siblings_id, gender, name, adocao_status)
+insert into crianca (id, address, age, civil_id, siblings_id, gender, name, adocao_status)
 values(1, 'rua da adoção', 11,'2020202020',1,'MASCULINO','Claudio', 'COMPLETED');
-insert into crianca  (id, address, age, civil_id, siblings_id, gender, name, adocao_status)
+insert into crianca (id, address, age, civil_id, siblings_id, gender, name, adocao_status)
 values(2, 'rua da adoção', 10,'20202021',2,'FEMININO','Joana', 'COMPLETED');
-insert into crianca  (id, address, age, civil_id, siblings_id, gender, name, adocao_status)
+insert into crianca (id, address, age, civil_id, siblings_id, gender, name, adocao_status)
 values(3, 'rua da adoção', 13,'80808416515',2,'FEMININO','Raquel', 'COMPLETED');
-insert into crianca  (id, address, age, civil_id, siblings_id, gender, name, adocao_status)
+insert into crianca (id, address, age, civil_id, siblings_id, gender, name, adocao_status)
 values(5, 'rua Z', 14,'15132150',1,'MASCULINO','Rodrigo', 'EMPTY');
-insert into crianca  (id, address, age, civil_id, siblings_id, gender, name, adocao_status)
+insert into crianca (id, address, age, civil_id, siblings_id, gender, name, adocao_status)
 values(6, 'rua Y', 9,'218044984',2,'FEMININO','Juliana', 'EMPTY');
-insert into crianca  (id, address, age, civil_id, siblings_id, gender, name, adocao_status)
+insert into crianca (id, address, age, civil_id, siblings_id, gender, name, adocao_status)
 values(7, 'rua X', 8,'808984',1,'FEMININO','Leilane', 'EMPTY');
-insert into crianca  (id, address, age, civil_id, siblings_id, gender, name, adocao_status)
+insert into crianca (id, address, age, civil_id, siblings_id, gender, name, adocao_status)
 values(8, 'rua X', 8,'808984',3,'FEMININO','Júlia', 'IN_PROGRESS');
-insert into crianca  (id, address, age, civil_id, siblings_id, gender, name, adocao_status)
+insert into crianca (id, address, age, civil_id, siblings_id, gender, name, adocao_status)
 values(9, 'rua X', 8,'808984',3,'FEMININO','Laís', 'IN_PROGRESS');
-insert into crianca  (id, address, age, civil_id, siblings_id, gender, name, adocao_status)
+insert into crianca (id, address, age, civil_id, siblings_id, gender, name, adocao_status)
 values(10, 'rua X', 8,'808984',3,'FEMININO','Pedro', 'IN_PROGRESS');
-
 
 insert into duvida (id, duvida, resposta, usuario_id)
 values(1, 'essa é a minha duvida', 'essa é minha resposta', 1);
