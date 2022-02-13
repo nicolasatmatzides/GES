9cfd0665c583485cad61f75cafa31fab46260151
EstherMattos <silvaesthermattos@hotmail.com>
2022-02-12 21:14:43 -0800

    testing

--- src/main/java/com/adocao/gpms/repository/CriancaRepository.java
+++ src/main/java/com/adocao/gpms/repository/CriancaRepository.java
@@ -3,14 +3,12 @@ package com.adocao.gpms.repository;
 import com.adocao.gpms.entity.Crianca;
 import com.adocao.gpms.model.AdocaoStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface CriancaRepository extends JpaRepository<Crianca, Long> {
-
 
     List<Crianca> findAllByAdocaoStatus(AdocaoStatus status);
 
